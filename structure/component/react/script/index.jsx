ff37c4b98034608b16afbe6e7362c06d467971e2
RamoFX <roman.dvorovoy123@gmail.com>
2020-12-12 00:59:40 +0100

    setup: structure - react component

---
+++ structure/component/react/script/index.jsx
@@ -0,0 +1,28 @@
+// Script frameworks & libraries
+import React from 'react'
+
+// Style frameworks & libraries
+/**/
+
+// Component dependencies
+// import ComponentName from 'ce/ComponentName'
+
+// Helpers
+/*__HELPER_PATTERN__*/import 'sh/__HELPER_NAME__'
+
+// Local dependencies
+import './style.__STYLE_EXTNAME__'
+
+
+
+// Main
+const __NAME__ = () => (
+  <div className="__NAME_LOWER__">
+    __NAME__
+  </div>
+)
+
+
+
+// Export
+export default __NAME__
