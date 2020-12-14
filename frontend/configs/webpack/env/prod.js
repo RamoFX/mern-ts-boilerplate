135559876c018b1b5b87687e66a074efd09dcecc
RamoFX <roman.dvorovoy123@gmail.com>
2020-12-14 20:59:30 +0100

    removed: webpack.optimize.DedupePlugin

--- frontend/configs/webpack/env/prod.js
+++ frontend/configs/webpack/env/prod.js
@@ -7,7 +7,6 @@ module.exports = {
     filename: '[name].bundle.min.js'
   },
   plugins: [
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new cleanWebpackPlugin.CleanWebpackPlugin()
   ]
