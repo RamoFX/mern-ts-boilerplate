80d1c0346ef5e3fb7d37c541d2a7c3d716b10e28
RamoFX <roman.dvorovoy123@gmail.com>
2020-12-15 16:31:10 +0100

    refactor: better helpers import

--- frontend/source/app/index.tsx
+++ frontend/source/app/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import './index.scss/'
+import './index.scss'
 
 import Root from 'main/Root'
 
-import { $ } from 'helpers/dom'
+import $ from 'helpers/dom'
 
 
 
