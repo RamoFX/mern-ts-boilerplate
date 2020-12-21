b45a003fe2a1d2e24b0f37f85d03d00712894e35
RamoFX <roman.dvorovoy123@gmail.com>
2020-12-21 18:56:03 +0100

    setup: API - Python, Flask

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,19 @@
-import flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-print('Hello from Python!')
+data = {
+  'dummy': ['foo', 'bar']
+}
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+
+@app.route('/api/dummy', methods=['GET'])
+def route_api_dummy():
+  return jsonify(data['dummy'])
+
+
+
+app.run(port = 8079)
--- frontend/source/app/ui/main/Root/index.tsx
+++ frontend/source/app/ui/main/Root/index.tsx
@@ -1,17 +1,46 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 
 import './style.scss'
 
 
 
-const Root: FC = () => (
-  <div className='root'>
-    <div className='top-bar'></div>
-    <div className='content'>
-      <span>Edit <pre>/mern-ts-boilerplate/frontend/source/app/ui/main/Root/index.tsx</pre> to see the changes!</span>
+const Root: FC = () => {
+  // Helpers
+  const path = (base: string, ...items: string[]): string => {
+    return [base, ...items].join('/')
+  }
+
+  // Main component
+  const editFilePath = 'mern-ts-boilerplate/frontend/source/app/ui/main/Root/index.tsx'
+
+  type TError = null | {message: string}
+  const [error, setError] = useState<TError>(null)
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState('')
+
+  const apiRoot = 'http://localhost:8079/api'
+
+  useEffect(() => {
+    fetch(path(apiRoot, 'dummy'))
+      .then( response => response.json() )
+      .then( data => {
+        setLoading(false)
+        setData(data)
+      } )
+      .catch( error => {
+        setLoading(false)
+        setError(error)
+      } )
+  }, [])
+
+  return <div className='root'>
+      <div className='top-bar'></div>
+      <div className='content'>
+        <span>Edit <pre>{ editFilePath }</pre> to see the changes!</span>
+        <span>Data from API: <pre>{ loading ? 'Fetching...' : error ? error!.message : data }</pre></span>
+      </div>
     </div>
-  </div>
-)
+}
 
 
 
